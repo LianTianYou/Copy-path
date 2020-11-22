7d5493a928f481b56364e94d2a58e3c6cba5d975
QAQ-Mdzz <74873576+QAQ-Mdzz@users.noreply.github.com>
2020-11-23 02:33:56 +0800

    Add files via upload

---
+++ 复制路径/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace 复制路径
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Clipboard.SetText(args[0]);
+        }
+    }
+}
